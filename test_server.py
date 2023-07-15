2d3861c689bd9c97bc18c33f70d04eae1222459d
Graph3x <hejzlar.filip@seznam.cz>
2023-07-15 21:09:24 +0200

    usage

--- test_server.py
+++ test_server.py
@@ -2,35 +2,19 @@ import socket
 
 
 def start_server(host, port):
-    # Create a TCP socket
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-    try:
-        # Bind the socket to the host and port
-        server_socket.bind((host, port))
-    except socket.error as e:
-        print(f"[!!] Failed to bind on {host}:{port}")
-        print(f"[!!] Error: {e}")
-        return
-
+    server_socket.bind((host, port))
     print(f"[*] Listening on {host}:{port}")
-
-    # Start listening for incoming connections
     server_socket.listen(5)
 
     while True:
-        # Accept a client connection
         client_socket, addr = server_socket.accept()
         print(f"[*] Accepted connection from {addr[0]}:{addr[1]}")
-
-        # Handle the client connection in a separate thread
         handle_client(client_socket)
 
 
 def handle_client(client_socket):
-    # Receive and send data in a loop until the client disconnects
     while True:
-        # Receive data from the client
         data = client_socket.recv(4096)
         if not data:
             break
@@ -52,7 +36,6 @@ def handle_client(client_socket):
         print(data)
         client_socket.send(b"asdfasdfasdfasdfasdf*!\n")
 
-    # Close the client socket
     client_socket.close()
     print("[*] Client disconnected")
 
